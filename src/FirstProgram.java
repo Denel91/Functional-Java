b8477ef19f84c2b6ae4cfaf63fb65154d16dfb0f
Daniele Di Salvo <disi91@hotmail.it>
2020-03-12 12:21:11 +0100

    Initial commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,30 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println(supToCil(2.6,3.4));
+
+        System.out.println(pluraleSm("Albero"));
+    }
+
+    /**
+     * @param r: raggio
+     * @param h: altezza
+     * @return
+     */
+    public static double supToCil(double r, double h) {
+
+        return (2 * Math.PI * (r + h));
+    }
+
+    /**
+     *
+     * @param s: a string value
+     * @return the plural name of a string
+     */
+    public static String pluraleSm(String s) {
+
+       return (s.substring(0, s.length()-1) + "i");
+    }
+}
+
+
