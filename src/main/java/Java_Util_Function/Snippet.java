ed7a8805f207626657e5f4cc8aa64e18da12ee6f
Daniele Di Salvo <disi91@hotmail.it>
2020-03-16 20:58:47 +0100

    Create Snippet

---
+++ src/main/java/Java_Util_Function/Snippet.java
@@ -0,0 +1,41 @@
+package Java_Util_Function;
+
+import java.util.function.*;
+
+public class Snippet {
+    public static void main(String[] args) {
+
+        // Interface Function<T,​R>
+        // Type Parameters:
+        // T - the type of the input to the function
+        // R - the type of the result of the function
+        Function<Integer, Double> function = (Integer a) -> a * 1.0;
+        System.out.println(function.apply(4));
+
+        // Interface Function<T,​R>
+        // Type Parameters:
+        // T - the type of the input to the function
+        // R - the type of the result of the function
+        Function<Integer, Double> myFunc = new Function<>() {
+            @Override
+            public Double apply(Integer integer) {
+                return integer * 10.0;
+            }
+        };
+
+        System.out.println(myFunc.apply(2));
+
+        // Interface Supplier<T>
+        // Type Parameters:
+        // T - the type of results supplied by this supplier
+        Supplier<String> mySupplier = new Supplier<String>() {
+            @Override
+            public String get() {
+                String res = "Success";
+                return res;
+            }
+        };
+
+        System.out.println(mySupplier.get());
+    }
+}
