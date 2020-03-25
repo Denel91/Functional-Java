38ed50972a333f23e347126bd856b1eb0ffe674c
Daniele Di Salvo <disi91@hotmail.it>
2020-03-25 21:00:25 +0100

    Update ListTest

--- src/main/java/Liste/ListTest.java
+++ src/main/java/Liste/ListTest.java
@@ -29,33 +29,12 @@ public class ListTest {
         intSList = intSList.cons(8);
         intSList = intSList.cons(3);
 
-        System.out.println("List -> " + intSList.toString());
+        intSList.display(intSList);
 
-        System.out.println("Legth -> " + intSList.length());
+        //System.out.println("List -> " + intSList.toString());
 
-        System.out.println(ListTest.test());
+        //System.out.println("Legth -> " + intSList.length());
+
+        //System.out.println(ListTest.test());
     }
 }
-
-/**
- *
- * Prova di esecuzione del metodo toString():
- *
- * intSList = {IntSList@791} "(3, 8, 7, 4)"
- *      empty = false
- *      first = 3
- *      rest = {IntSList@834} "(8, 7, 4)"
- *          empty = false
- *          first = 8
- *          rest = {IntSList@836} "(7, 4)"
- *              empty = false
- *              first = 7
- *              rest = {IntSList@838} "(4)"
- *                  empty = false
- *                  first = 4
- *                  rest = {IntSList@840} "(---)"
- *                      empty = true
- *                      first = 0
- *                      rest = null
- */
-
