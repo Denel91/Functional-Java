635a09ca62b312800ebdd4e23e198ef3dd2eb7fb
Daniele Di Salvo <disi91@hotmail.it>
2020-03-29 20:45:03 +0200

    Implemented method: isEmpty(), peek(), pop(), push()

--- src/main/java/Data_Structures/Stack.java
+++ src/main/java/Data_Structures/Stack.java
@@ -2,6 +2,8 @@ package Data_Structures;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.EmptyStackException;
+import java.util.Objects;
 
 /**
  * Definizione della classe Stack<E> che rappresenta una pila nella quale gli elementi
@@ -9,8 +11,8 @@ import java.util.List;
  * ogni elemento che viene inserito va in cima alla pila e l'ultimo inserito è anche il primo a uscirne.
  *
  * Class overview:
+ * size(): int
  * isEmpty(): boolean
- * top() : Object
  * peek():  Object
  * pop() : Object
  * push(Object e) : void
@@ -23,21 +25,96 @@ import java.util.List;
  */
 
 public class Stack<E> {
-    private int top;
-    private List<E> elements;
-    private static final int DEFAULT_SIZE = 5;
+    // variabili di istanza
+    private int top;  // the index of the item at the top of the stack
+    private List<E> elements; // the items of the stack
+    private static final int DEFAULT_SIZE = 10; // default stack size
 
+    /**
+     * Default Constructor
+     */
     public Stack() {
         this(DEFAULT_SIZE);
     }
 
-    public Stack(int size) {
-        size = size == 0 ? DEFAULT_SIZE : size;
+    /**
+     * Constructor (Creator).
+     *
+     * @param capacity the size of the stack. REQUIRE >= 0
+     */
+    public Stack(int capacity) {
+        capacity = capacity == 0 ? DEFAULT_SIZE : capacity;
         this.top = -1; // Stack inizialmente vuoto
-        this.elements = new ArrayList<>(size);
+        this.elements = new ArrayList<>(capacity);
     }
 
+    /**
+     * @return the size of the stack
+     */
     public int size() {
         return this.elements.size();
+    }
+
+    /**
+     * @return true if this stack contains no elements.
+     */
+    public boolean isEmpty() {
+        return top < 0;
+    }
+
+    /**
+     * @return the object on top of the stack without taking it out
+     * @throws EmptyStackException if isEmpty() is true
+     */
+    public E peek() throws EmptyStackException {
+        if (isEmpty())
+            throw new EmptyStackException();
+
+        return elements.get(top);
+    }
+
+    /**
+     * Delete the item located at the top of the stack
+     *
+     * @return the item at the top of the stack
+     * @throws EmptyStackException if isEmpty() is true
+     */
+    public E pop() throws EmptyStackException {
+        Objects.requireNonNull(this.elements);
+        E item;
+        if (isEmpty())
+            throw new EmptyStackException();
+
+        item = elements.remove(top);
+        assert (top == elements.size() - 1);
+        return item;
+    }
+
+    /**
+     * Insert the item on top of the stack
+     *
+     * @param item the object to insert on top of the stack
+     */
+    public void push(E item) {
+        Objects.requireNonNull(this.elements);
+        elements.add(++top, item);
+        assert (top == elements.size() - 1);
+    }
+
+    public static void main(String[] args) {
+
+        Stack<Integer> myStack = new Stack<>(5);
+
+        System.out.println(myStack.isEmpty()); // true
+        System.out.println(myStack.size()); // 0
+        myStack.push(5);
+        myStack.push(6);
+        myStack.push(8);
+        myStack.push(2);
+        myStack.push(4);
+        myStack.push(3);
+        System.out.println(myStack.size()); // 6
+        System.out.println(myStack.pop()); // 3
+        System.out.println(myStack.size()); // 5
     }
 }
