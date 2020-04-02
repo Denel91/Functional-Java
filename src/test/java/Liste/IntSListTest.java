a712af48bd061c1feb62fcd2d52ee9056610f07d
Daniele Di Salvo <disi91@hotmail.it>
2020-04-02 19:56:23 +0200

    Update junit tests in IntSListTest

--- src/test/java/Liste/IntSListTest.java
+++ src/test/java/Liste/IntSListTest.java
@@ -1,6 +1,7 @@
 package Liste;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runners.Parameterized.Parameter;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -24,5 +25,66 @@ class IntSListTest {
         sList = new IntSList(5, sList);
         sList = sList.cons(3);
         assertEquals(3, sList.car());
+    }
+
+    @Test
+    void  cdr() {
+        list = new IntSList(7, list);
+        list = list.cons(4);
+        list = list.cons(6);
+        list = list.cons(9);
+        assertEquals(list.cdr(), list.cdr());
+    }
+
+    @Parameter(7)
+    public int cInput;
+    @Parameter(7)
+    public int cExpected;
+    @Test
+    void cons() {
+        list = new IntSList(cInput, list);
+        assertEquals(cExpected, list.car());
+    }
+
+    @Test
+    void length() {
+        list = new IntSList(7, list);
+        list = list.cons(4);
+        list = list.cons(6);
+        list = list.cons(9);
+        assertEquals(4, list.cdr().length() + 1);
+    }
+
+    @Parameter(2)
+    public int index;
+    @Test
+    void listRef() {
+        list = new IntSList(7, list);
+        list = list.cons(4);
+        list = list.cons(6);
+        list = list.cons(9);
+
+        if (index == 0) {
+            assertEquals(9, list.car());
+
+        } else {
+            assertEquals(6, list.cdr().listRef(index - 1));
+        }
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void append() {
+    }
+
+    @Test
+    void reverse() {
+    }
+
+    @Test
+    void testToString() {
     }
 }
