4133a84c813895a9869814f7569744ca2dc75f13
Daniele Di Salvo <disi91@hotmail.it>
2020-04-03 20:04:01 +0200

    JUnit test class for StringSList

---
+++ src/test/java/Liste/StringSListTest.java
@@ -0,0 +1,80 @@
+package Liste;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for StringSList
+ *
+ * @version 03/04/2020
+ */
+class StringSListTest {
+
+    // Liste per i test
+    private StringSList sList = new StringSList();
+    private StringSList list = new StringSList();
+    private StringSList ListString = new StringSList();
+    private StringSList singly = new StringSList();
+
+    @Test
+    @DisplayName("isNull")
+    void isEmpty() {
+    }
+
+    @Test
+    @DisplayName("car")
+    void car() {
+    }
+
+    @Test
+    @DisplayName("cdr")
+    void cdr() {
+    }
+
+    @Test
+    @DisplayName("cons")
+    void cons() {
+    }
+
+    @Test
+    @DisplayName("length")
+    void length() {
+    }
+
+    @Test
+    @DisplayName("listRef")
+    void listRef() {
+    }
+
+    @Test
+    @DisplayName("testEquals")
+    void testEquals() {
+    }
+
+    @Test
+    @DisplayName("append")
+    void append() {
+    }
+
+    @Test
+    @DisplayName("reverse")
+    void reverse() {
+    }
+
+    @Test
+    @DisplayName("ToString")
+    void testToString() {
+    }
+
+    @Test
+    @DisplayName("display")
+    void display() {
+    }
+
+    @Test
+    @DisplayName("fromArray")
+    void fromArray() {
+    }
+}
