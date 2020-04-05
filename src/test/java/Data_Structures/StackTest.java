180fe22f6d05db5e050070f28bebb9f4e615191d
Daniele Di Salvo <disi91@hotmail.it>
2020-04-05 22:59:29 +0200

    Create class StackTest

---
+++ src/test/java/Data_Structures/StackTest.java
@@ -0,0 +1,84 @@
+package Data_Structures;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StackTest {
+
+    @Test
+    @DisplayName("size")
+    void size() {
+    }
+
+    @Test
+    @DisplayName("DefaultSize")
+    void getDefaultSize() {
+    }
+
+    @Test
+    @DisplayName("getCapacity")
+    void getCapacity() {
+    }
+
+    @Test
+    @DisplayName("setCapacity")
+    void setCapacity() {
+    }
+
+    @Test
+    @DisplayName("getElements")
+    void getElements() {
+    }
+
+    @Test
+    @DisplayName("isEmpty")
+    void isEmpty() {
+    }
+
+    @Test
+    @DisplayName("peek")
+    void peek() {
+    }
+
+    @Test
+    @DisplayName("pop")
+    void pop() {
+    }
+
+    @Test
+    @DisplayName("push")
+    void push() {
+    }
+
+    @Test
+    @DisplayName("contains")
+    void contains() {
+    }
+
+    @Test
+    @DisplayName("search")
+    void search() {
+    }
+
+    @Test
+    @DisplayName("clear")
+    void clear() {
+    }
+
+    @Test
+    @DisplayName("Clone")
+    void testClone() {
+    }
+
+    @Test
+    @DisplayName("Equals")
+    void testEquals() {
+    }
+
+    @Test
+    @DisplayName("ToString")
+    void testToString() {
+    }
+}
