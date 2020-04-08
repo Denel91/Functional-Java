13ddab6317c8f0ef09e7a7ee9303bc2d501ec8cc
Daniele Di Salvo <disi91@hotmail.it>
2020-04-08 19:31:45 +0200

    Implemented class StackTest

--- src/test/java/Data_Structures/StackTest.java
+++ src/test/java/Data_Structures/StackTest.java
@@ -1,11 +1,9 @@
 package Data_Structures;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 
 class StackTest {
 
@@ -24,12 +22,9 @@ class StackTest {
     void getCapacity() {
     }
 
+    @Test
     @DisplayName("setCapacity")
-    @ParameterizedTest(name = "For example, the value {0} is the new capacity of the stack.")
-    @ValueSource(ints = {5, 8, 12, 17})
-    void setCapacity(int capacity) {
-        Stack<Integer> stack = new Stack<Integer>(capacity);
-        assertEquals(capacity, stack.getCapacity());
+    void setCapacity() {
     }
 
     @Test
