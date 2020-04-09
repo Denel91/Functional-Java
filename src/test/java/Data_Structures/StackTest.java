9d0694253c290aabd2371b3d1c8ae507b66431e5
Daniele Di Salvo <disi91@hotmail.it>
2020-04-09 14:31:24 +0200

    Implemented class StackTest

--- src/test/java/Data_Structures/StackTest.java
+++ src/test/java/Data_Structures/StackTest.java
@@ -1,84 +1,171 @@
 package Data_Structures;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-
+/**
+ * Class Test
+ */
 class StackTest {
 
-    @Test
-    @DisplayName("size")
-    void size() {
-    }
+    Stack<Integer> stack;
+    Stack<Integer> myStack;
 
     @Test
-    @DisplayName("DefaultSize")
-    void getDefaultSize() {
+    @DisplayName("newStackTest")
+    void newStackTest() {
+        new Stack<Integer>();
+        new Stack<Integer>();
     }
 
-    @Test
-    @DisplayName("getCapacity")
-    void getCapacity() {
-    }
+    @Nested
+    @DisplayName("Class afterNewStack")
+    class afterNewStack {
 
-    @Test
-    @DisplayName("setCapacity")
-    void setCapacity() {
-    }
+        // Executed before each @Test
+        @BeforeEach
+        void newStack() {
+            stack = new Stack<Integer>();
+        }
 
-    @Test
-    @DisplayName("getElements")
-    void getElements() {
-    }
+        @Test
+        @DisplayName("isEmpty")
+        void isEmpty() {
+            assertTrue(stack.isEmpty(), () -> "Empty");
+        }
 
-    @Test
-    @DisplayName("isEmpty")
-    void isEmpty() {
-    }
+        @Test
+        @DisplayName("DefaultSize")
+        void getDefaultSize() {
+            assertEquals(10, stack.getDefaultSize());
+        }
 
-    @Test
-    @DisplayName("peek")
-    void peek() {
-    }
+        @Test
+        @DisplayName("getCapacity")
+        void getCapacity() {
+            assertEquals(10, stack.getCapacity());
+        }
 
-    @Test
-    @DisplayName("pop")
-    void pop() {
-    }
+        @ParameterizedTest(name = "For example, the value {0} is the new capacity of the stack.")
+        @ValueSource(ints = {6, 8, 12, 15})
+        @DisplayName("setCapacity")
+        void setCapacity(int capacity) {
+            myStack = new Stack<Integer>(capacity);
+            assertEquals(capacity, myStack.getCapacity());
+        }
 
-    @Test
-    @DisplayName("push")
-    void push() {
-    }
+        @Test
+        @DisplayName("testEquals")
+        void testEquals() {
+            myStack = new Stack<Integer>();
+            assertTrue(myStack.equals(stack), () -> "Equals");
+        }
 
-    @Test
-    @DisplayName("contains")
-    void contains() {
-    }
+        @Nested
+        @DisplayName("Class afterPushing")
+        class afterPushing {
 
-    @Test
-    @DisplayName("search")
-    void search() {
-    }
+            Integer number = 8;
 
-    @Test
-    @DisplayName("clear")
-    void clear() {
-    }
+            // Executed before each @Test
+            @BeforeEach
+            @DisplayName("pushElementStack")
+            void pushElement() {
+                stack.push(number);
+            }
 
-    @Test
-    @DisplayName("Clone")
-    void testClone() {
-    }
+            @Test
+            @DisplayName("isNotEmpty")
+            void isNotEmpty() {
+                assertFalse(stack.isEmpty(), () -> "Not Empty");
+            }
 
-    @Test
-    @DisplayName("Equals")
-    void testEquals() {
-    }
+            @Test
+            @DisplayName("size")
+            void size() {
+                assertEquals(1, stack.size());
+            }
 
-    @Test
-    @DisplayName("ToString")
-    void testToString() {
+            @Test
+            @DisplayName("getElements")
+            void getElements() {
+                assertAll("properties", () -> {
+                    assertFalse(stack.isEmpty(), () -> "Not Empty");
+
+                    // Executed only if the previous assertion is valid.
+                    assertAll("elements", () -> assertEquals(stack.getElements(), stack.getElements()));
+                });
+            }
+
+            @Test
+            @DisplayName("peek")
+            void peek() {
+                assertAll("properties", () -> {
+                    assertFalse(stack.isEmpty(), () -> "Not Empty");
+
+                    // Executed only if the previous assertion is valid.
+                    assertAll("first element ", () -> assertEquals(8, stack.peek()));
+                });
+            }
+
+            @Test
+            @DisplayName("pop")
+            void pop() {
+                assertEquals(number, stack.pop());
+                assertTrue(stack.isEmpty(), () -> "Popped");
+            }
+
+            @Test
+            @DisplayName("contains")
+            void contains() {
+                assertFalse(stack.isEmpty(), () -> "Not Empty");
+                assertTrue(stack.contains(number), () -> "OK");
+            }
+
+            @Test
+            @DisplayName("search")
+            void search() {
+                assertFalse(stack.isEmpty(), () -> "Not Empty");
+                assertEquals(0, stack.search(number));
+            }
+
+            @Test
+            @DisplayName("clear")
+            void clear() {
+                stack.clear();
+                assertEquals(0, stack.size());
+                assertTrue(stack.isEmpty(), () -> "OK");
+            }
+
+            @Test
+            @DisplayName("clone")
+            void testClone() {
+                myStack = (Stack<Integer>) stack.clone();
+                assertTrue(myStack.contains(number), () -> "OK");
+                assertEquals(1, myStack.size());
+                assertFalse(myStack.isEmpty(), () -> "Not Empty");
+            }
+
+            @Test
+            @DisplayName("equals")
+            void testEqualsAfterPush() {
+                myStack = new Stack<Integer>();
+                myStack.push(number);
+                assertTrue(stack.equals(myStack), () -> "Equals");
+            }
+
+            @Test
+            @DisplayName("toString")
+            void testToString() {
+                String message = stack.toString();
+                assertEquals(stack.toString(), message);
+            }
+        }
     }
 }
