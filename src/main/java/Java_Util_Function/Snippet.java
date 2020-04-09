67d736097a5ee8bf7af4d6d1ad35c42fd2f447ec
Daniele <disi91@hotmail.it>
2020-04-09 19:44:45 +0200

    Implemented filter() and reduce()

--- src/main/java/Java_Util_Function/Snippet.java
+++ src/main/java/Java_Util_Function/Snippet.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.BiFunction;
 
 /**
  * ATTENZIONE: Questa classe non fa parte delle lezioni.
@@ -49,13 +50,26 @@ public class Snippet {
         System.out.println(mySupplier.get());
 
         List<Integer> list = new ArrayList<Integer>();
+        list.add(4);
+        list.add(3);
+        list.add(6);
+        list.add(5);
         list.add(1);
         list.add(2);
-        list.add(3);
-        list = map(list, x -> x * 2);
 
+        list = map(list, x -> x * 2);
+        System.out.print("Map: ");
         for (Integer val : list)
             System.out.print(val + " ");
+
+        list = filter(list, x -> x >= 0);
+        System.out.print("Filter: ");
+        for (Integer val : list)
+            System.out.print(val + " ");
+
+        System.out.println("Reduce: ");
+        int sum = reduce(list, (a, b) -> a + b, 0);
+        System.out.println("sum: " + sum);
 
         double positivo = 5.0;
         double negativo = -5.0;
@@ -75,5 +89,25 @@ public class Snippet {
         }
 
         return mappedList;
+    }
+
+    static <T, R> List<T> filter(List<T> list, Function<T, R> function) {
+        List<T> filteredList = new ArrayList<T>();
+        for (T element : list) {
+            if ((boolean) function.apply(element)) {
+                filteredList.add(element);
+            }
+        }
+
+        return filteredList;
+    }
+
+    static <T> T reduce(List<T> list, BiFunction<T, T, T> function, T initial) {
+        T accumulator = initial;
+        for (T element : list) {
+            accumulator = function.apply(accumulator, element);
+        }
+
+        return accumulator;
     }
 }
