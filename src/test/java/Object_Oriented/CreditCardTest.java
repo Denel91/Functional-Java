69e16db43519cdad8e2fbc69e489f6593af3cb06
Daniele Di Salvo <disi91@hotmail.it>
2020-04-10 19:34:49 +0200

    Implemented class CreditCard for tests

---
+++ src/test/java/Object_Oriented/CreditCardTest.java
@@ -0,0 +1,47 @@
+package Object_Oriented;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+/**
+ * Class Test
+ */
+class CreditCardTest {
+
+    @Test
+    @DisplayName("")
+    void getCustomer() {
+    }
+
+    @Test
+    @DisplayName("")
+    void getBank() {
+    }
+
+    @Test
+    @DisplayName("")
+    void getAccount() {
+    }
+
+    @Test
+    @DisplayName("")
+    void getLimit() {
+    }
+
+    @Test
+    @DisplayName("")
+    void getBalance() {
+    }
+
+    @Test
+    @DisplayName("")
+    void charge() {
+    }
+
+    @Test
+    @DisplayName("")
+    void makePayment() {
+    }
+}
