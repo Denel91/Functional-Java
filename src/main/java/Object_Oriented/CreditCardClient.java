4e44770a3737c89036625d0b4d70778bd13f6569
Daniele Di Salvo <disi91@hotmail.it>
2020-04-10 19:33:38 +0200

    Implemented class CreditCardClient [main]

---
+++ src/main/java/Object_Oriented/CreditCardClient.java
@@ -0,0 +1,19 @@
+package Object_Oriented;
+
+public class CreditCardClient {
+    public static void main(String[] args) {
+
+        CreditCard creditCard = new Object_Oriented.CreditCard("Alessandro Bowman", "Deutsche Bank Italia", "5391 " +
+                "0375 9387 8309", 3000, 1250);
+
+        CreditCard.printSummary(creditCard);
+
+        creditCard.charge(100);
+
+        System.out.println("New balance = " + creditCard.getBalance()); // 1350.0
+
+        creditCard.makePayment(350);
+
+        System.out.println("New balance = " + creditCard.getBalance()); // 1000.0
+    }
+}
