abba51fbf9761add1f62ed956ac5c13c1e94f9cb
Daniele Di Salvo <disi91@hotmail.it>
2020-04-19 11:51:23 +0200

    Update protocol of class Queens

--- src/main/java/N_Regine/Queens.java
+++ src/main/java/N_Regine/Queens.java
@@ -34,10 +34,50 @@ public class Queens {
         }
     }
 
+    /*
+     * Numero di soluzioni:
+     *
+     * Il numero di modi diversi in cui si possono disporre n regine
+     * in una scacchiera n x n è dato dal numero di modi diversi in
+     * cui si può completare la disposizione delle regine:
+     *
+     * --> numberOfCompletions(new Board(n))
+     *
+     * a partire da una scacchiera n x n inizialmente vuota
+     */
     public static int numeroDiSoluzioni(int n) {
         return numeroDiCompletamenti(new Board(n));
     }
 
+    /*
+     * Il numero di modi in cui si può completare la disposizione
+     * a partire da una scacchiera b parzialmente configurata
+     *
+     * --> numberOfCompletions(b) : int
+     *
+     * dove k regine (0 <= k < n) sono collocate nelle prime k righe
+     * di b, si può determinare a partire dalle configurazioni
+     * che si ottengono aggiungendo una regina nella riga k + 1 in tutti
+     * i modi possibili (nelle posizioni che non sono già minacciate)
+     *
+     * for ( int j = 1; j <= n; j = j+1 ) {
+     *   if (!b.underAttack(i,j)) {
+     *          ... b.addQueen(i,j) ...
+     *      }
+     * }
+     *
+     * calcolando ricorsivamente per ciascuna di queste il numero
+     * di modi in cui si può completare la disposizione
+     *
+     * numberOfCompletions(b.addQueen(i,j))
+     *
+     * e sommando i valori che ne risultano
+     * count = count + numberOfCompletions(...)
+     *
+     * Se invece la scacchiera rappresenta una soluzione (q == n)
+     * c'è un solo modo (banale) di completare la disposizione:
+     * lasciare le cose come stanno!
+     */
     private static int numeroDiCompletamenti(Board b) {
 
         int n = b.size();
@@ -94,9 +134,9 @@ public class Queens {
         list = new BoardSList(c, list);
         list = list.cons(t);
 
-        System.out.println(c.addQueen(3,2));        // Board {[ b3 ]}
-        System.out.println(c.addQueen(3,3));        // Board {[ c3 ]}
-        System.out.println(c.insertQueen(3,4));    // Board {[ d3 ]}
+        System.out.println(c.addQueen(3, 2));        // Board {[ b3 ]}
+        System.out.println(c.addQueen(3, 3));        // Board {[ c3 ]}
+        System.out.println(c.insertQueen(3, 4));    // Board {[ d3 ]}
 
         // una lista vuota
         BoardSList sList = new BoardSList();
