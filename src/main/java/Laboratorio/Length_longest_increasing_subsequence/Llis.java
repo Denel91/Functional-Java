c4c01492cd1bf9ac10dccbabb6f95527a3a75a80
Daniele Di Salvo <disi91@hotmail.it>
2020-05-05 14:34:33 +0200

    Update class Llis

--- src/main/java/Laboratorio/Length_longest_increasing_subsequence/Llis.java
+++ src/main/java/Laboratorio/Length_longest_increasing_subsequence/Llis.java
@@ -1,7 +1,11 @@
 package Laboratorio.Length_longest_increasing_subsequence;
 
+import Liste.IntSList;
+
 /**
  * class length of the longest increasing subsequence
+ *
+ * @version 05/05/2020
  */
 public class Llis {
 
@@ -9,13 +13,30 @@ public class Llis {
     private static final int UNKNOWN = 0;
 
     public static void main(String[] args) {
-        // Calcola la lunghezza della più lunga sottosequenza di s strettamente crescente
+        // Calcola la lunghezza della più lunga sottosequenza di s strettamente crescente (Versione Ricorsiva)
+        System.out.println("Versione ricorsiva: ");
         System.out.println(llis(new int[]{5, 4, 3, 2, 1})); // 1
+        System.out.println(llis(new int[]{3, 10, 2, 1, 20})); // 3
+        System.out.println(llis(new int[]{7, 38, 39, 25, 44})); // 4
+        System.out.println(llis(new int[]{27, 90, 7, 29, 49, 8, 53, 1, 28, 6})); // 4
+        System.out.println(llis(new int[]{9, 46, 54, 71, 60, 47, 0, 32, 25, 61})); // 5
+        System.out.println(llis(new int[]{54, 52, 42, 33, 14, 40, 37, 61, 53, 1})); // 3
+
+        // Calcola la lunghezza della più lunga sottosequenza di s strettamente crescente (Versione Memoization)
+        System.out.println("Versione memoization: ");
+        System.out.println(llisMemoization(new int[]{3, 10, 2, 1, 20})); // 3
+
+        /*
         System.out.println(llisMemoization(new int[]{5, 4, 3, 2, 1})); // 1
         System.out.println(llisMemoization(new int[]{47, 38, 39, 25, 44})); // 3
         System.out.println(llisMemoization(new int[]{27, 90, 7, 29, 49, 8, 53, 1, 28, 6})); // 4
         System.out.println(llisMemoization(new int[]{9, 46, 54, 71, 60, 47, 0, 32, 25, 61})); // 5
         System.out.println(llisMemoization(new int[]{54, 52, 42, 33, 14, 40, 37, 61, 53, 1})); // 3
+        System.out.println(llisMemoization(new int[]{1, 2, 3, 4, 5})); // 5
+        System.out.println(llisMemoization(new int[]{5, 4, 8})); // 2
+        System.out.println(llisMemoization(new int[]{50, 3, 10, 7, 40, 80})); // 4
+        System.out.println(llisMemoization(new int[]{10, 22, 9, 33, 21, 50, 41, 60, 80})); // 6
+         */
     }
 
     /**
@@ -60,10 +81,12 @@ public class Llis {
      */
     public static int llisMemoization(int[] s) {
         final int n = s.length;
-        final int[] h = new int[n + 1];
+        final int[][] h = new int[n + 1][n + 1];
 
-        for (int i = 0; i <= n; i++) {
-            h[i] = UNKNOWN;
+        for (int x = 0; x <= n; x++) {
+            for (int y = 0; y <= n; y++) {
+                h[x][y] = UNKNOWN;
+            }
         }
 
         return llisMem(s, 0, 0, h);
@@ -75,22 +98,98 @@ public class Llis {
      *
      * @param s an array of Integers
      * @param i an index
-     * @param t an index
+     * @param j an index
      * @param h an array to store the values
      * @return la lunghezza della più lunga sottosequenza di s strettamente crescente
      */
-    private static int llisMem(int[] s, int i, int t, int[] h) {
-        final int n = s.length;
+    private static int llisMem(int[] s, int i, int j, int[][] h) {
+        final int n = s.length; // s: {3, 10, 2, 1, 20}
+        // sequenza estesa con un elemento in più di posizione n e valore 0
+        int[] p = new int[n + 1]; // p: {0, 0, 0, 0, 0, 0}
+
+        int t = p[j]; //-> ArrayIndexOutOfBoundsException
+
         if (i == n) {
-            h[i] = 0;
+            h[i][j] = 0;
 
         } else if (s[i] <= t) {
-            h[i] = llisRec(s, i + 1, t);
+            h[i][j] = llisMem(s, i + 1, t, h);
 
         } else {
-            h[i] = Math.max(1 + llisRec(s, i + 1, s[i]), llisRec(s, i + 1, t));
+            h[i][j] = Math.max(1 + llisMem(s, i + 1, s[i], h), llisMem(s, i + 1, t, h));
+
         }
 
-        return h[i];
+        return h[i][j];
+    }
+
+    /**
+     * Longest increasing subsequence
+     * Versione memoization
+     *
+     * @param s
+     * @return
+     */
+    public static IntSList lis(int[] s) {
+        final int n = s.length;
+        final IntSList[][] h = new IntSList[n + 1][n + 1];
+
+        for (int x = 0; x <= n; x++) {
+            for (int y = 0; y <= n; y++) {
+                h[x][y] = IntSList.NULL_INTLIST;
+            }
+        }
+
+        return lisMem(s, 0, 0, h);
+    }
+
+    /**
+     * Procedura di supporto per longest increasing subsequence
+     * Versione memoization
+     *
+     * @param s
+     * @param i
+     * @param j
+     * @param h
+     * @return
+     */
+    private static IntSList lisMem(int[] s, int i, int j, IntSList[][] h) {
+        // s: {3, 10, 2, 1, 20} i: 0 t: 0 list: (-)
+        final int n = s.length; // n = 5
+        int[] p = new int[n + 1];
+
+        int t = p[j];
+
+        if (i == n) {
+            h[i][j] = IntSList.NULL_INTLIST;
+
+        } else if (s[i] <= t) {
+            h[i][j] = lisMem(s, i + 1, t, h);
+
+        } else {
+            h[i][j] = longer(lisMem(s, i + 1, s[i], h).cons(s[i]), lisMem(s, i + 1, t, h));
+
+        }
+
+        return h[i][j];
+    }
+
+    private static IntSList longer(IntSList a, IntSList b) {
+        int m = a.length();
+        int n = b.length();
+
+        if (m < n) {
+            return b;
+
+        } else if (m > n) {
+            return a;
+
+        } else if (Math.random() < 0.5) {
+            return b;
+
+        } else {
+            return a;
+        }
     }
 }
+
