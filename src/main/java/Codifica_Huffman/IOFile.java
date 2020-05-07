a24d7a30a9c3337645971cb198955d28e7b605c3
Daniele Di Salvo <disi91@hotmail.it>
2020-05-07 15:48:57 +0200

    Implemented class IOFile

---
+++ src/main/java/Codifica_Huffman/IOFile.java
@@ -0,0 +1,34 @@
+package Codifica_Huffman;
+
+import huffman_toolkit.InputTextFile;
+import huffman_toolkit.OutputTextFile;
+
+/**
+ * class IOFile
+ */
+public class IOFile {
+
+    /**
+     * Copia il contenuto del file src nel file dst
+     *
+     * @param src file sorgente
+     * @param dst file destinazione
+     * @return the lines's number in dst
+     */
+    public static int copyFile(String src, String dst) {
+        InputTextFile in = new InputTextFile(src);
+        OutputTextFile out = new OutputTextFile(dst);
+        int count = 0;
+
+        while (in.textAvailable()) {
+            String line = in.readTextLine();
+            out.writeTextLine(line);
+            count += 1;
+        }
+
+        in.close();
+        out.close();
+
+        return count;
+    }
+}
