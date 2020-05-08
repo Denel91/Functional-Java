214fa3dea08ee69c725b9d0d35c424651ee6a7d8
Daniele Di Salvo <disi91@hotmail.it>
2020-05-08 13:25:31 +0200

    Upadate folder

--- src/main/java/Codifica_Huffman/IOTest.java
+++ src/main/java/Codifica_Huffman/IOTest.java
@@ -2,7 +2,7 @@ package Codifica_Huffman;
 
 public class IOTest {
     public static void main(String[] args) {
-      int value =  IOFile.copyFile("IOFile", "FileOutput");
+      int value = IOFile.copyFile("FileInput.txt", "FileOutput.txt");
       System.out.println(value);
     }
 }
