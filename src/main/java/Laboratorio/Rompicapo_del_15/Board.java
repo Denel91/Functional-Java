aca12acf87326320c848db33da0f6709c507fc21
Daniele Di Salvo <disi91@hotmail.it>
2020-05-13 15:34:37 +0200

    Fixed isSorted() method

--- src/main/java/Laboratorio/Rompicapo_del_15/Board.java
+++ src/main/java/Laboratorio/Rompicapo_del_15/Board.java
@@ -79,16 +79,12 @@ public class Board {
     public boolean isSorted() {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (board[i][j] > board[i][j + 1] ) {
+                if (board[i][j] > board[i][j + 1])
                     return false;
-
-                } else {
-                    return true;
-                }
             }
         }
 
-        return false;
+        return true;
     }
 
     /**
