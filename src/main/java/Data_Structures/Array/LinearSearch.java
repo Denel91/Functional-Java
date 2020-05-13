ff42b7d776269b1fe28a117ef6c891ec01968f85
Daniele Di Salvo <disi91@hotmail.it>
2020-05-13 15:47:25 +0200

    Implemented method isSorted()

--- src/main/java/Data_Structures/Array/LinearSearch.java
+++ src/main/java/Data_Structures/Array/LinearSearch.java
@@ -1,19 +1,26 @@
 package Data_Structures.Array;
 
 import java.util.Arrays;
+
 public class LinearSearch {
     public static void main(String[] args) {
         int[] values = {2, 5, 8, 1, 10, 22, 9, 33, 21, 50, 41, 60, 80};
+        int[] vec = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30};
         ricercaLineare(values, 33); // 7
+        boolean res = isSorted(values);
+        boolean response = isSorted(vec);
+        System.out.println(res); // false
+        System.out.println(response); // true
         Arrays.sort(values);
         System.out.println(Arrays.toString(values)); // [1, 2, 5, 8, 9, 10, 21, 22, 33, 41, 50, 60, 80]
+        System.out.println(Arrays.toString(vec)); // [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30]
         ricercaLineare(values, 33); // 8
     }
 
     /**
      * Ricerca lineare
      *
-     * @param array un array di Integers
+     * @param array  un array di Integers
      * @param chiave l'elemento da cercare nell'array
      * @return l'indice di posizione dell'elemento
      */
@@ -38,5 +45,15 @@ public class LinearSearch {
         }
 
         return pos;
+    }
+
+    public static boolean isSorted(int[] vector) {
+        for (int i = 0; i < vector.length - 1; i++) {
+            if (vector[i] > vector[i + 1]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
