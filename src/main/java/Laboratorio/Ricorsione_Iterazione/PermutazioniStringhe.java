ef1d91c53a78cd4683bf5faebba7390e434bb4c3
Daniele Di Salvo <disi91@hotmail.it>
2020-06-15 13:29:38 +0200

    Implemented class PermutazioniStringhe

---
+++ src/main/java/Laboratorio/Ricorsione_Iterazione/PermutazioniStringhe.java
@@ -0,0 +1,55 @@
+package Laboratorio.Ricorsione_Iterazione;
+
+import java.util.Vector;
+import java.util.Stack;
+
+public class PermutazioniStringhe {
+
+    public static Vector<String> perm(String u) {
+        Vector<String> p = new Vector<String>();
+        permRec(u, 0, p);
+        return p;
+    }
+
+    public static void permRec(String u, int k, Vector<String> p) {
+        int n = u.length();
+        if (k == n - 1) {
+            p.add(u);
+        } else {
+            permRec(u, k + 1, p);
+            for (int i = k + 1; i < n; i = i + 1) {
+                String v = u.substring(0, k) + u.substring(i, i + 1) + u.substring(k + 1, i) + u.substring(k, k + 1) + u.substring(i + 1, n);
+                permRec(v, k + 1, p);
+            }
+        }
+    }
+
+    public static Vector<String> permIter(String u) {
+        Vector<String> p = new Vector<String>();
+        int n = u.length(), k;
+        Stack<String> s = new Stack<String>();
+        Stack<Integer> t = new Stack<Integer>();
+        s.push(u);
+        t.push(0);
+
+        do {
+            k = t.pop();
+
+            if (k == n - 1) {
+                p.add(u);
+
+            } else {
+
+
+            }
+
+        } while (!s.isEmpty());
+
+        return p;
+    }
+
+    public static void main(String[] args) {
+        Vector<String> v = perm("ci");
+        System.out.println(v.toString());
+    }
+}
