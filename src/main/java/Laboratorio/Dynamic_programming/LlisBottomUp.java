26415ed9d0b5bda722da10030d88d0f352e4c6c2
Daniele Di Salvo <disi91@hotmail.it>
2020-07-13 15:10:54 +0200

    Implemented class LlisBottomUp

--- src/main/java/Laboratorio/Dynamic_programming/LlisBottomUp.java
+++ src/main/java/Laboratorio/Dynamic_programming/LlisBottomUp.java
@@ -4,7 +4,7 @@ import java.util.Vector;
 
 public class LlisBottomUp {
     // Variabile di classe
-    private static final int UNKNOWN = 0;
+    private static final int UNKNOWN = -1;
 
     public static int llis(Vector<Integer> s) {
         return llisRec(s, 0, 0);
@@ -15,8 +15,10 @@ public class LlisBottomUp {
     public static int llisRec(Vector<Integer> s, int t, int k) {
         if (k == s.size()) {
             return 0;
+
         } else if (s.get(k) <= t) {
             return llisRec(s, t, k + 1);
+
         } else {
             return Math.max(1 + llisRec(s, s.get(k), k + 1), llisRec(s, t, k + 1));
         }
@@ -25,48 +27,26 @@ public class LlisBottomUp {
     //------------ Versione Bottom-Up ------------//
 
     public static int llisDP(Vector<Integer> s, int t, int k) {
-        Vector<Integer> b = new Vector<>(s.size() + 1);
+        int n = s.size(); // 5
+        Vector<Integer> b = new Vector<>(n + 1);
 
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < n; i++) {
             b.add(s.get(i));
         }
 
         b.add(0); // b -> [3, 10, 2, 1, 20, 0]
 
-        int[][] h = new int[b.size() + 1][b.size() + 1];
+        int m = b.size(); // n + 1 --> 6
 
-        for (int x = 0; x <= t; x++) {
-            h[x][b.size()] = 0;
+        int[][] h = new int[m][m];
+
+        for (int x = 0; x <= n; x++) {
+            h[x][n] = 0;
         }
 
-        for (int y = 0; y <= k; y++) {
-            h[b.size()][y] = 0;
-        }
-
-
-        /**
-         *
-         * Matrice h riempita con i casi base.
-         *
-         *     0   1   2   3   4   5   6
-         * 0 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 1 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 2 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 3 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 4 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 5 |   |   |   |   |   |   | 0 |
-         *   -   -   -   -   -   -   -   -
-         * 6 | 0 | 0 | 0 | 0 | 0 | 0 | 0 |
-         */
-
-        for (int i = k - 1; i >= 0; i--) {
-            for (int j = 0; j <= b.size(); j++) {
-                if (b.get(j) <= b.get(j + 1)) {
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= n; j++) {
+                if (b.get(i) <= b.get(j)) {
                     h[j][i] = h[j][i + 1];
                 } else {
                     h[j][i] = Math.max(1 + h[i][i + 1], h[j][i + 1]);
@@ -74,7 +54,7 @@ public class LlisBottomUp {
             }
         }
 
-        return h[t][k];
+        return h[n][0];
     }
 
     //------------ Memoization ------------//
@@ -83,8 +63,10 @@ public class LlisBottomUp {
         if (h[t][k] == UNKNOWN) {
             if (k == s.size()) {
                 h[t][k] = 0;
+
             } else if (s.get(k) <= s.get(t)) {
                 h[t][k] = llisMem(s, t, k + 1, h);
+
             } else {
                 h[t][k] = Math.max(1 + llisMem(s, k, k + 1, h), llisMem(s, t, k + 1, h));
             }
@@ -122,6 +104,6 @@ public class LlisBottomUp {
         System.out.println(a.toString()); // [3, 10, 2, 1, 20]
         System.out.println(llis(a)); // 3 -> (3, 10, 20)
         System.out.println(llisMemoization(a)); // 3 -> (3, 10, 20)
-        System.out.println(llisDP(a, 0, 0));
+        System.out.println(llisDP(a, 0, 0)); // 3
     }
 }
